ae765ed1f810ac1bbc720a59e8235746736f3e2e
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-07-17 13:59:21 +0530

    fixed some issue

--- Small_Projects/students_identity.py
+++ Small_Projects/students_identity.py
@@ -1,8 +1,5 @@
 # In this project there will be a dictionary in a list that will tell us students identity by the roll number given to then ie the index number of the dict in the list 
 
-
-
-
 DataLst=[' ',{"Name-":"Raj","Class-":12,"Roll no.-":12,"School-":"DAV Public School","Email-":"rajdav@xyz.in"},
 {"Name-":"John","Class-":12,"Roll no.-":42,"School-":"XYZ Public School","Email-":"johnxyz@xyz.in"}, {"Name-":"Mike","Class-":12,"Roll no.-":23,"School-":"Peter Public School","Email-":"mike@peter.in"}]
 print('Welcome, pls press any key to continue')
@@ -12,7 +9,7 @@ print("===========================================")
 def function():
     roll_no= int(input("Enter roll number-")) 
     if roll_no != 0:
-        print('Enter Roll number-')
+        print()
  
     else:
         print('Pls recheck and enter the correct roll no.')
@@ -21,7 +18,7 @@ def function():
     print(DataLst[roll_no]) 
     print("Thank you")
 
-    again= input('Again? Type y/n-')
+    again= input('Again? Type (yes/no) y/n-')
     if again == 'y':
         function()
     else:
