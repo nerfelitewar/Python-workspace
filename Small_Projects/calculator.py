c5127829c402b33491a7f9c2cf92028c7c3f6734
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-07-17 13:51:17 +0530

    did a small project ie students identity

---
+++ Small_Projects/calculator.py
@@ -0,0 +1,4 @@
+#making basic simple calculator
+#with functions like- 
+'''Addition, Sub, multiply, divison , power, root, trignometric, logratimic, matrix, determinatant, etc'''
+
---
+++ Small_Projects/students_identity.py
@@ -0,0 +1,34 @@
+# In this project there will be a dictionary in a list that will tell us students identity by the roll number given to then ie the index number of the dict in the list 
+
+
+
+
+DataLst=[' ',{"Name-":"Raj","Class-":12,"Roll no.-":12,"School-":"DAV Public School","Email-":"rajdav@xyz.in"},
+{"Name-":"John","Class-":12,"Roll no.-":42,"School-":"XYZ Public School","Email-":"johnxyz@xyz.in"}, {"Name-":"Mike","Class-":12,"Roll no.-":23,"School-":"Peter Public School","Email-":"mike@peter.in"}]
+print('Welcome, pls press any key to continue')
+input()
+print("===========================================")
+
+def function():
+    roll_no= int(input("Enter roll number-")) 
+    if roll_no != 0:
+        print('Enter Roll number-')
+ 
+    else:
+        print('Pls recheck and enter the correct roll no.')
+        function()
+
+    print(DataLst[roll_no]) 
+    print("Thank you")
+
+    again= input('Again? Type y/n-')
+    if again == 'y':
+        function()
+    else:
+        pass 
+
+
+
+function()
+    
+
