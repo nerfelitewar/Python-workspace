8f48c052eac3670ced7f9e49ebbe324d792cf0a3
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-09-12 03:58:44 +0530

    DOne

--- python_qna/Q11.py
+++ python_qna/Q11.py
@@ -1,24 +1,15 @@
 '''Program to print a prime and not a prime in a list if a lst is 1 return not a prime and composite'''
 #taking a sample list 
-lst=[1,2,3]
-num=0
-x=1
-while x>0:
-   num+=1
-   if num<=len(lst):
-      pass
-countNum=0
-while x>0:
-    for i in range(2,num):
-         for LstNum in lst:
-            if LstNum==1:
-                print(LstNum,'Neither a prime nor a composite')
-            if LstNum%i==0:
-                print(LstNum,'is not prime')
-            else:
-                print(LstNum,'is prime')
-            countNum=countNum+1
-            if countNum==num:
-                  pass 
-         break
-    break 
+lst=[0,1,2,3]
+for i in lst:
+    if i == 1 or i==0: 
+        print(f'{i} Neither a prime nor a composite')
+        continue
+    
+
+    for j in range(2, i):
+        if i % j == 0:
+            print(f'{i} is not prime')
+            break
+    else:
+        print(f'{i} is prime')
