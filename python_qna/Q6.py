acdfa35dcaad5dc771189529e1e52723c096dbbf
nerfelitewar <80751691+nerfelitewar@users.noreply.github.com>
2022-11-07 20:29:10 +0530

    Delete python_qna directory

--- python_qna/Q6.py
+++
@@ -1,7 +0,0 @@
-'''
-search duplicates numbers in a list and return the number  with index no.
-'''
-L=[1,1,2,3,4,5,6,7,5,9]
-for idOfDub, i in enumerate(L):
-    if L.count(i)!=1:
-        print(i,idOfDub, end='<---INDEX ')
