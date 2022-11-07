81efbc30beb5fea38847779925ba3aa2ec386a50
nerfelitewar <md.arshadrazanoori23@gmail.com>
2022-11-07 20:25:21 +0530

    done

--- Python_Projects/Prime_composite.py
+++ Python_Projects/Prime_composite.py
@@ -1,34 +1,15 @@
+my_list=[1,2,3,4,5,6,7,8,15]
+prime=[]
+composite=[]
 
-
-## prime composite number function from a list.
-# x=int(input('Enter numbers-'))
-# x=list(x)
-x=[1,2,3,4,5,6,7,13,15,20,21]
-def prime_comp(list):
-    primes=[]
-    composites=[]
-    count=0
-    for num in list:
-        # num=num
-    
-        i=1
-        while i<=len(x):
-            if i%num==0:
-                count=count+1
-            i=i+1
-
-            if count==2:
-                return 'Prime numbers-', primes.append(num)
-            elif count!=2:
-                return 'Composite numbers-', composites.append(num)
-            else:
-                return 'Neither prime nor composite numbers-',num
-
-
-prime_comp(x)
-
-### THIS SHIT IS BROKEN AHHHHHHHHHHHHH HELP ME BRUH
-
-            
-
-
+for i in my_list :
+    c=0
+    for j in range(1,i): #iterate from 1 as 1 and 0 are not prime or composites
+        if i%j==0:
+            c+=1
+    if c>1:
+        composite.append(i)
+    if c==1:
+        prime.append(i)
+print('primes-',prime)
+print('composites-',composite)
